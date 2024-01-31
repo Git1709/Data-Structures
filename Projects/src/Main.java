6706ae04f634b17d1f24e97f44eafb9e448ec47a
git1709 <rachit22csu306@ncuindia.edu>
2024-01-31 21:33:54 +0530

    Day 1

---
+++ Projects/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    /*
+    Create an array of integer with size n. Return the difference between the largest and the smallest value inside that array.
+*/
+
+    public static void main(String[] args) {
+        int Array_size;
+
+        System.out.println("Enter the Size of the Array : ");
+        Scanner UserIn = new Scanner(System.in);
+        Array_size= UserIn.nextInt();
+        int[] array1= new int[Array_size];
+        System.out.println("Enter elements of array : ");
+        for(int i=0;i<Array_size;i++){
+            array1[i]= UserIn.nextInt();
+        }
+        Arrays.sort(array1);
+        System.out.println("Minimum element is : "+array1[0]+"\nMaximum element is : "+array1[Array_size-1]);
+        System.out.println("Difference between minimum and maximum elements in array is : "+ (array1[Array_size-1] -array1[0]));
+    }
+
+}
---
+++ Projects/src/ParenthesisCheck.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class ParenthesisCheck {
+}
+
+class BalancedParentheses {
+    public static boolean areParenthesesBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        // Iterate through each character in the string
+        for (char ch : str.toCharArray()) {
+            // If an opening parenthesis is encountered, push it onto the stack
+            if (ch == '('||ch == '{'||ch == '[') {
+                stack.push(ch);
+            }
+            // If a closing parenthesis is encountered
+            else if (ch == ')'|| ch == '}'|| ch == ']') {
+                // If the stack is empty, there's no matching opening parenthesis
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                // Pop the top element from the stack and check if it matches the closing parenthesis
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') || (ch == '}' && top != '{')||  (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        // After processing the entire string, if the stack is empty, all parentheses are balanced
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter expression:");
+        Scanner sc= new Scanner(System.in);
+        String str = sc.next();
+        if (areParenthesesBalanced(str)) {
+            System.out.println("Parentheses Balanced");
+        } else {
+            System.out.println("Parentheses Not Balanced");
+        }
+    }
+}
